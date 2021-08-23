317fb7e52f3672ea80d169400975f6915e1cf447
bobbylight <robert.e.futrell@gmail.com>
2021-08-23 00:07:53 -0400

    Adding a single Edisen test to see what happens

--- src/test/java/org/fife/edisen/ui/EdisenTest.java
+++ src/test/java/org/fife/edisen/ui/EdisenTest.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.IOException;
 
 @ExtendWith(SwingRunnerExtension.class)
-@Disabled("Doesn't play nicely running in GitHub Actions environment")
 public class EdisenTest {
 
     private Edisen edisen;
@@ -40,24 +39,28 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testCreateMenuBar() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.createMenuBar(edisen.getPreferences()));
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testCreateSplashScreen() {
         edisen = createEdisen();
         Assertions.assertNull(edisen.createSplashScreen());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testCreateStatusBar() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.createStatusBar(edisen.getPreferences()));
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testCreateToolBar() {
         edisen = createEdisen();
         Assertions.assertNull(edisen.createToolBar(edisen.getPreferences()));
@@ -76,6 +79,7 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetSetAssemblerCommandLine() throws IOException {
 
         edisen = createEdisen();
@@ -97,6 +101,7 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetSetEmulatorCommandLine() throws IOException {
 
         edisen = createEdisen();
@@ -118,12 +123,14 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetFileChooser() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getFileChooser());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetSetLinkerCommandLine() throws IOException {
 
         edisen = createEdisen();
@@ -145,18 +152,21 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetOptionsDialog() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getOptionsDialog());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetPreferences() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getPreferences());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetProject() throws IOException {
 
         edisen = createEdisen();
@@ -175,36 +185,42 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetRecentFiles() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getRecentFiles());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetRecentProjects() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getRecentProjects());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetResourceBundleClassName() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getResourceBundleClassName());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetSelectedTabIndex() {
         edisen = createEdisen();
         Assertions.assertEquals(0, edisen.getSelectedTabIndex());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetTheme() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getTheme());
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testGetVersionString() {
         edisen = createEdisen();
         Assertions.assertNotNull(edisen.getVersionString());
@@ -217,12 +233,14 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testLog() {
         edisen = createEdisen();
         edisen.log("stdout", "Test message");
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testRefreshLookAndFeel() {
         edisen = createEdisen();
         Assertions.assertNotEquals(Theme.NORD, edisen.getTheme());
@@ -231,6 +249,7 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testSaveAllDirtyFiles_success() throws IOException {
 
         edisen = createEdisen();
@@ -253,6 +272,7 @@ public class EdisenTest {
     }
 
     @Test
+    @Disabled("Doesn't play nicely running in GitHub Actions environment")
     public void testSaveCurrentFile() throws IOException {
 
         edisen = createEdisen();
